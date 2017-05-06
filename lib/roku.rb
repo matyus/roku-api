801fc8ac9a4cd3c39dfa35855dbf33bb8bc9c0e5
Michael Matyus <michael@maty.us>
2017-05-06 14:23:18 -0400

    add roku library

---
+++ lib/roku.rb
@@ -0,0 +1,21 @@
+class Roku
+  include HTTParty
+
+  def initialize(roku_box, debug = true)
+    self.class.debug_output if debug
+
+    self.class.base_uri roku_box.base_uri
+  end
+
+  def keypress(command)
+    self.class.post("/keypress/#{command}")
+  end
+
+  def apps
+    self.class.get("/query/apps").parsed_response
+  end
+
+  def active_app
+    self.class.get("/query/active-app").parsed_response
+  end
+end
